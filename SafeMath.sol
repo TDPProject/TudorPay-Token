f5ce17d37dc8071c62c3ee519a27c1b76857bfad
TDPProject <33520473+TDPProject@users.noreply.github.com>
2017-12-20 01:39:06 +0800

    SafeMATH.sol

---
+++ SafeMath.sol
@@ -0,0 +1,42 @@
+/* SafeMATH.sol
+   
+   This file is part of ERC223 mintable token.
+
+   Token is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+pragma solidity ^0.4.18;
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+
+        assert(c >= a);
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        assert(b <= a);
+
+        c = a - b;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a * b;
+
+        assert(c / a == b);
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a / b;
+    }
+}
